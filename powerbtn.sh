2aad5bb275a9e11a01f610e26cd79783995808e9
ta264 <tom@andrewsmail.co.uk>
2015-01-25 08:32:45 +0000

    Restart kodi when power button pressed

--- powerbtn.sh
+++ powerbtn.sh
@@ -2,16 +2,7 @@
 # /etc/acpi/powerbtn.sh
 # When power button is pressed, check if mythtv is busy before allowing shutdown
 
-logger "power button pressed"
-
-# run as 'tom' else mythshutdown will barf
-su tom -c "checkshutdown"
-status=$?
-logger "status is $status"
-
-if [ $status -eq 0 ]; then
-    logger "shutdown allowed"
-    poweroff
-else
-    logger "shutdown is not allowed"
-fi
+logger "power button pressed, killing Kodi and restarting lightdm"
+killall -SIGKILL kodi.bin
+service lightdm restart
+logger "lightdm restarted"
