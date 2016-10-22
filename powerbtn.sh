1041900e4668fd022640892b85682e4254578361
Tom Andrews <tom@andrewsmail.co.uk>
2016-10-22 13:26:15 +0100

    Restart kodi service not lightdm

--- powerbtn.sh
+++ powerbtn.sh
@@ -2,7 +2,6 @@
 # /etc/acpi/powerbtn.sh
 # When power button is pressed, check if mythtv is busy before allowing shutdown
 
-logger "power button pressed, killing Kodi and restarting lightdm"
-killall -SIGKILL kodi.bin
-service lightdm restart
-logger "lightdm restarted"
+logger "power button pressed, killing Kodi and restarting"
+service kodi restart
+logger "kodi restarted"
